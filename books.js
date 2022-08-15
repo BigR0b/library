f539eca41a171dbdd20431e8fb4c5fd8c4e8e080
Robin Rhodes <bigr0b@protonmail.com>
2022-08-15 15:00:25 -0400

    Add function to properly display all books in myLibrary array on the page

--- books.js
+++ books.js
@@ -2,12 +2,15 @@
 
 let myLibrary = [];
 const newBookBtn = document.querySelector(".newBookBtn");
+const createBookBtn = document.querySelector(".create");
+
 const libraryContainer = document.querySelector(".container");
 const title = document.querySelector(".title");
 const author = document.querySelector(".author");
 const pages = document.querySelector(".pages");
 const hasRead = document.querySelector(".hasRead");
 const submitBtn = document.querySelector(".submit");
+const bookForm = document.querySelector(".form-container");
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -15,11 +18,11 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
   this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, Has read : ${this.read}`;
+    return `${this.title},  ${this.author}, ${this.pages} pages Has read : ${this.read}`;
   };
 }
 
-function createBook() {
+function addBookToLibrary() {
   let newBook = new Book(
     title.value,
     author.value,
@@ -29,6 +32,25 @@ function createBook() {
   myLibrary.push(newBook);
 }
 
-newBookBtn.addEventListener("click", createBook);
+function displayBooks(arr) {
+  while (libraryContainer.firstChild) {
+    libraryContainer.removeChild(libraryContainer.firstChild);
+  }
+  arr.forEach((element) => {
+    let book = document.createElement("div");
+    book.innerText = element.info();
+    libraryContainer.appendChild(book);
+  });
+}
+function displayForm() {
+  bookForm.classList.toggle("hidden");
+}
+newBookBtn.addEventListener("click", displayForm);
+createBookBtn.addEventListener("click", addBookToLibrary);
+createBookBtn.addEventListener("click", function () {
+  displayBooks(myLibrary);
+  displayForm();
+});
+
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 console.log(theHobbit.info());
