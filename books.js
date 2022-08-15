2f3e0088b4441428b57e0b0b3a9d0321df32ee2c
Robin Rhodes <bigr0b@protonmail.com>
2022-08-15 00:46:35 -0400

    Add a function that adds a object from the inputs
    made a function that takes all the inputs of the form and creates a object out of them and puts them into a array when you click the button

--- books.js
+++ books.js
@@ -1,19 +1,34 @@
 "use strict";
 
-let myLibary = [];
+let myLibrary = [];
 const newBookBtn = document.querySelector(".newBookBtn");
 const libraryContainer = document.querySelector(".container");
+const title = document.querySelector(".title");
+const author = document.querySelector(".author");
+const pages = document.querySelector(".pages");
+const hasRead = document.querySelector(".hasRead");
+const submitBtn = document.querySelector(".submit");
 
-newBookBtn.addEventListener("click");
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
   this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+    return `${this.title} by ${this.author}, ${this.pages} pages, Has read : ${this.read}`;
   };
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+function createBook() {
+  let newBook = new Book(
+    title.value,
+    author.value,
+    pages.value,
+    hasRead.checked
+  );
+  myLibrary.push(newBook);
+}
+
+newBookBtn.addEventListener("click", createBook);
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 console.log(theHobbit.info());
